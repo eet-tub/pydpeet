097c034a82578c6a5ddd1b772de866125350e814
schlanto <145531424+schlanto@users.noreply.github.com>
2025-06-03 14:59:52 +0200

    initial arbin_old test

---
+++ test/arbin_old/test_reader.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import Mock
+
+from numpy.ma.testutils import assert_equal
+
+from ppb.zyklisierer.arbin_old.reader import to_DataFrame, read_sheets
+
+
+class ArbinOldTests(unittest.TestCase):
+    def test_invalid_input_path(self):
+        for value in ["invalid", None, ""]:
+            try:
+                _, _ = to_DataFrame(value)
+                self.fail()
+            except FileNotFoundError:
+                pass
+            except ValueError:
+                pass
+
+    def test_read_sheets(self):
+
+        mock_obj = Mock()
+        mock_obj.sheet_names = []
+
+        df, md = read_sheets(mock_obj)
+
+        assert_equal(df, None)
+        assert_equal(md, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
